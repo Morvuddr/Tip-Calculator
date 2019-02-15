037347c4079381b2fb1b65809e920ed826194970
Igor Bopp <igor_bopp@mail.ru>
2019-02-15 23:28:35 +0300

    Added all needed views.

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    @IBOutlet weak var inputCardView: UIView!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var outputCardView: UIView!
+    @IBOutlet weak var tipAmountTitleLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountTitleLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        if sender.isOn{
+            print ("ON")
+        } else {
+            print("Off")
+        }
+    }
+    
+    @IBAction func tipPercentChanged(_ sender: Any) {
+    }
+    
+    
+    @IBAction func resetButtonTapped(_ sender: Any) {
+    }
+    
 }
 
